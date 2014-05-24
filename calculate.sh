d0191ce7bdf3a901ad62a519e89603476bed8e6e
Takashi Unuma @ QSMCS <kijima.m.u@gmail.com>
2014-05-24 18:59:38 +0900

    食費、電車代の合計を個別出力するように変更

--- calculate.sh
+++ calculate.sh
@@ -3,7 +3,7 @@
 # calculate.sh
 #
 # original script coded by Takashi Unuma, Kyoto Univ.
-# Last modified: 2014/01/07
+# Last modified: 2014/05/24
 #
 
 if test $# -lt 1 ; then
@@ -16,3 +16,5 @@ infile=$1
 awk '{print $0}' ${infile}
 echo "---------------------------"
 awk 'NR>3 {sum += $2}END{printf ("Total: %12d\n", sum)}' ${infile}
+awk 'NR>3 && /食費/   {sum += $2}END{printf ("食費: %13d\n", sum)}' ${infile}
+awk 'NR>3 && /電車代/ {sum += $2}END{printf ("電車代: %11d\n", sum)}' ${infile}
